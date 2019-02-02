66c611d1046aa845e7353c045bc9ff20ce434d7d
k1nd0ne <k1nd0ne@mbp-de-felix.home>
2019-02-02 19:03:02 +0100

    Layout des bouttons

--- SlitherProject/src/launcher/Launcher.java
+++ SlitherProject/src/launcher/Launcher.java
@@ -2,6 +2,7 @@ package launcher;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.AbstractButton;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -19,26 +20,31 @@ public class Launcher {
 	
 	
 	public Launcher() {
-		fenetre = new JFrame("---Menu principal---");
-		fenetre.setSize(400, 200);
+		fenetre = new JFrame("Menu principal");
+		fenetre.setSize(200, 300);
 		panel = new JPanel();
 		fenetre.setContentPane(panel);
 		JLabel titre = new JLabel("---SLITHER GAME---");
 		b1 = new JButton("PlayerVsIA");
 		b2 = new JButton("PlayerVsPlayer");
-		b3 = new JButton("Graphe aleatoire");
+		b3 = new JButton("Random Graphe");
+		
+		b1.setLayout(null);
+		b2.setLayout(null);
+		b3.setLayout(null);
+		
+		b1.setLocation(200, 200);
 		
 		b3.addActionListener(new ActionListener() {
-			
-			@Override
 			public void actionPerformed(ActionEvent e) {
 				Game g = new Game();
 				g.render();
 				g.play();
-				System.out.println("jeu fini");
-				
+				System.out.println("jeu fini");	
 			}
 		});
+		//Faire les prochains Listeners
+	    
 		fenetre.getContentPane().add(titre);
 		fenetre.getContentPane().add(b1);
 		fenetre.getContentPane().add(b2);

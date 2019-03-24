2a474d30e763da65bb45bdc35f9f7689b8dcd120
k1nd0ne <k1nd0ne@mbp-de-felix.home>
2019-03-24 20:10:28 +0100

    Ajout IA

--- SlitherProject/src/launcher/Launcher.java
+++ SlitherProject/src/launcher/Launcher.java
@@ -33,6 +33,7 @@ public class Launcher{
 		
 		quit.addActionListener(new ActionListener() {
 			@Override
+			
 			public void actionPerformed(ActionEvent e) {
 				fenetre.setVisible(false);
 				System.exit(0);
